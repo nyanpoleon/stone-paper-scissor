4ddfbfc7b6a57f00846a02efe8af6c6d618545cc
Purujit Pradhan <purujit26@gmail.com>
2023-03-16 17:41:28 +0530

    functionality to rock and paper

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const elementToHide = document.getElementById('hidethis');
 const rulesButton = document.getElementById('ruleshere')
 
 hideButton.addEventListener('click', function () {
-  elementToHide.style.display = 'none';
+    elementToHide.style.display = 'none';
 });
 
 function displayRules() {
-  elementToHide.style.display = 'block';
+    elementToHide.style.display = 'block';
 };
 
 //WHEN PLAYGAIN IN CLICKED
@@ -36,6 +36,10 @@ function playagain() {
             
             
         </div>`
+
+    document.getElementById("testBtn").innerHTML = `
+    <button class="rules-button1" id="ruleshere" style=" margin-top: 499px;
+    margin-left: 1360px;" onclick="displayRules()">RULES</button>`
 }
 
 
@@ -48,7 +52,7 @@ let scoreYour = document.querySelector("#your");
 let scoreComp = document.querySelector("#comp");
 
 //fetch score of local storage
-let playerScore = document.getElementById('your').innerHTML = localStorage.getItem("YOUR SCORE") || 0; 
+let playerScore = document.getElementById('your').innerHTML = localStorage.getItem("YOUR SCORE") || 0;
 let computerScore = document.getElementById('comp').innerHTML = localStorage.getItem("COMPUTER SCORE") || 0;
 
 
@@ -59,9 +63,9 @@ let scissors = document.querySelector(".purple");
 
 // ROCK
 function handleRock() {
-  const verdict = gameplay(0); //0  is rock
-  if(verdict === 'tie') {
-    document.getElementById("dynamic").innerHTML= `
+    const verdict = gameplay(0); //0  is rock
+    if (verdict === 'tie') {
+        document.getElementById("dynamic").innerHTML = `
     <div class="ka-iba-ibo">
             <div><img class="ripple1" src="files/ripple.svg" alt="" style="opacity:0%"></div>
             <div class="player-move">
@@ -81,9 +85,11 @@ function handleRock() {
                 <img src="files/${nameofMove}-big.svg" alt="">
             </div>
         </div>`;
-  } else if (verdict === 'user') {
-      document.getElementById("dynamic").innerHTML= `
-      <div class="ka-iba-ibo">
+
+
+    } else if (verdict === 'user') {
+        document.getElementById("dynamic").innerHTML = `
+        <div class="ka-iba-ibo">
                 <div><img class="ripple1" src="files/ripple.svg" alt="" ></div>
                 <div class="player-move">
                     
@@ -101,8 +107,12 @@ function handleRock() {
                     <div class="picks">PC PICKED</div>
                     <img src="files/${nameofMove}-big.svg" alt="">
                 </div>`
-  } else if (verdict === 'computer') {
-      document.getElementById("dynamic").innerHTML= `
+
+        document.getElementById("testBtn").innerHTML = `
+                <button class="rules-button1" id="ruleshere" onclick="displayRules()">RULES</button>
+                <a href="hurray.html"><button class="nextBtn">NEXT</button></a>`
+    } else if (verdict === 'computer') {
+        document.getElementById("dynamic").innerHTML = `
       <div class="ka-iba-ibo">
                 <div><img class="ripple1" src="files/ripple.svg" alt="" style="opacity:0%"></div>
                 <div class="player-move">
@@ -123,15 +133,15 @@ function handleRock() {
                 </div>`
         document.getElementById("testBtn").innerHTML = `
             <button class="rules-button1" id="ruleshere" style="">RULES</button>`
-  }
-  
+    }
+
 }
 
 //PAPER
 function handlePaper() {
-  const verdict = gameplay(1); //1 is paper
-  if(verdict === 'tie'){
-    document.getElementById("dynamic").innerHTML= `
+    const verdict = gameplay(1); //1 is paper
+    if (verdict === 'tie') {
+        document.getElementById("dynamic").innerHTML = `
     <div class="ka-iba-ibo">
               <div><img class="ripple1" src="files/ripple.svg" alt="" style="opacity:0%"></div>
               <div class="player-move">
@@ -151,9 +161,9 @@ function handlePaper() {
                   <img src="files/${nameofMove}-big.svg" alt="">
               </div>
           </div>`;
-          
-  } else if( verdict === 'user') {
-      document.getElementById("dynamic").innerHTML= `
+
+    } else if (verdict === 'user') {
+        document.getElementById("dynamic").innerHTML = `
       <div class="ka-iba-ibo">
                 <div><img class="ripple1" src="files/ripple.svg" alt="" ></div>
                 <div class="player-move">
@@ -173,8 +183,12 @@ function handlePaper() {
                     <img src="files/${nameofMove}-big.svg" alt="">
                 </div>
             </div>`;
-  } else if(verdict === 'computer') {
-    document.getElementById("dynamic").innerHTML= `
+
+        document.getElementById("testBtn").innerHTML = `
+            <button class="rules-button1" id="ruleshere" onclick="displayRules()">RULES</button>
+            <a href="hurray.html"><button class="nextBtn">NEXT</button></a>`
+    } else if (verdict === 'computer') {
+        document.getElementById("dynamic").innerHTML = `
       <div class="ka-iba-ibo">
                 <div><img class="ripple1" src="files/ripple.svg" alt="" style="opacity:0%"></div>
                 <div class="player-move">
@@ -194,15 +208,15 @@ function handlePaper() {
                     <img src="files/${nameofMove}-big.svg" alt="">
                 </div>
             </div>`;
-  }
+    }
 
 }
 
 //SCISSORS
 function handleScissor() {
-  const verdict = gameplay(2); //2 is scissors
-  if(verdict === 'tie') {
-    document.getElementById("dynamic").innerHTML= `
+    const verdict = gameplay(2); //2 is scissors
+    if (verdict === 'tie') {
+        document.getElementById("dynamic").innerHTML = `
     <div class="ka-iba-ibo">
               <div><img class="ripple1" src="files/ripple.svg" alt="" style="opacity:0%"></div>
               <div class="player-move">
@@ -222,8 +236,8 @@ function handleScissor() {
                   <img src="files/${nameofMove}-big.svg" alt="">
               </div>
           </div>`;
-  } else if(verdict === 'user') {
-      document.getElementById("dynamic").innerHTML= `
+    } else if (verdict === 'user') {
+        document.getElementById("dynamic").innerHTML = `
       <div class="ka-iba-ibo">
                 <div><img class="ripple1" src="files/ripple.svg" alt=""></div>
                 <div class="player-move">
@@ -247,8 +261,8 @@ function handleScissor() {
         document.getElementById("testBtn").innerHTML = `
             <button class="rules-button1" id="ruleshere" onclick="displayRules()">RULES</button>
             <a href="hurray.html"><button class="nextBtn">NEXT</button></a>`
-  } else if(verdict === 'computer') {
-      document.getElementById("dynamic").innerHTML= `
+    } else if (verdict === 'computer') {
+        document.getElementById("dynamic").innerHTML = `
       <div class="ka-iba-ibo">
                 <div><img class="ripple1" src="files/ripple.svg" alt="" style="opacity:0%"></div>
                 <div class="player-move">
@@ -268,8 +282,8 @@ function handleScissor() {
                     <img src="files/${nameofMove}-big.svg" alt="">
                 </div>
             </div>`;
-  }
-  
+    }
+
 }
 
 var computer_move;
@@ -277,31 +291,31 @@ var nameofMove;
 //THE GAMEPLAY LOGIC
 function gameplay(player_move) {
 
-  computer_move = Math.floor(Math.random() * moves.length);
-  nameofMove = moves[computer_move];
-  console.log(nameofMove);
+    computer_move = Math.floor(Math.random() * moves.length);
+    nameofMove = moves[computer_move];
+    console.log(nameofMove);
 
-  //IF ITS A TIE
-  if (player_move === computer_move) {
-    return 'tie';
-  } 
+    //IF ITS A TIE
+    if (player_move === computer_move) {
+        return 'tie';
+    }
 
-  //CONDITONS FOR PLAYER WIN
-  else if ((player_move === 0 && computer_move === 2) || 
-    (player_move === 1 && computer_move === 0) || 
-    (player_move === 2 && computer_move === 1)) {
+    //CONDITONS FOR PLAYER WIN
+    else if ((player_move === 0 && computer_move === 2) ||
+        (player_move === 1 && computer_move === 0) ||
+        (player_move === 2 && computer_move === 1)) {
 
-      playerScore++;
-      scoreYour.textContent = `${playerScore}`; // your counter realtime update
-      localStorage.setItem("YOUR SCORE", playerScore); //localstorage
-      return 'user' ;
+        playerScore++;
+        scoreYour.textContent = `${playerScore}`; // your counter realtime update
+        localStorage.setItem("YOUR SCORE", playerScore); //localstorage
+        return 'user';
 
-  //CONDITIONS FOR PLAYER LOSS
-  } else {
+        //CONDITIONS FOR PLAYER LOSS
+    } else {
 
-    computerScore++;
-    scoreComp.textContent = `${computerScore}`; //computer counter realtime update
-    localStorage.setItem("COMPUTER SCORE", computerScore); //localstorage
-    return 'computer';
-  }
+        computerScore++;
+        scoreComp.textContent = `${computerScore}`; //computer counter realtime update
+        localStorage.setItem("COMPUTER SCORE", computerScore); //localstorage
+        return 'computer';
+    }
 }
